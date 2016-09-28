400e8ef4cf2eb258f1b4bc9050170445ef3b5b0c
Stela <stela.d.dineva@gmail.com>
2016-09-28 22:49:01 +0300

    Add models and migrations for researcher application

--- researcher/models.py
+++ researcher/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class University(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Researcher(models.Model):
+    user = models.ForeignKey(User, related_name='researchers')
+    university = models.ForeignKey(University, related_name='researchers')
+    scientific_degree = models.CharField(max_length=255)
