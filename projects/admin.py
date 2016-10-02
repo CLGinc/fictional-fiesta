47e8c7c7262de2164bd9d703ff6cc36ad9272e7e
George Goranov <g.p.goranov@gmail.com>
2016-10-02 10:39:31 +0300

    add projects models to admin site

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Source, Project, Role
+
+admin.site.register(Source)
+admin.site.register(Project)
+admin.site.register(Role)
