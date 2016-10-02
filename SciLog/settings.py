d5581f9f9f2678bbcb56e5bb21f91978e8588799
George Goranov <g.p.goranov@gmail.com>
2016-10-02 10:30:44 +0300

    add media settings

--- SciLog/settings.py
+++ SciLog/settings.py
@@ -126,3 +126,8 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 STATIC_URL = '/static/'
+
+# Media files (user images)
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
+MEDIA_URL = '/media/'
