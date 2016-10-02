678bfdc5dfe436af16cd31503c840256ef8f5b3a
George Goranov <g.p.goranov@gmail.com>
2016-10-02 10:36:12 +0300

    add protocols models to admin site

--- protocols/admin.py
+++ protocols/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Protocol, Asset, Procedure
+from .models import Step, Result, Attachment, DataColumn
+
+admin.site.register(Protocol)
+admin.site.register(Asset)
+admin.site.register(Procedure)
+admin.site.register(Step)
+admin.site.register(Result)
+admin.site.register(Attachment)
+admin.site.register(DataColumn)
