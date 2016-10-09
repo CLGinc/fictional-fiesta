a5ae5e48cbe2b00e90be5d5daa849808dbe29db2
George Goranov <g.p.goranov@gmail.com>
2016-10-09 12:35:18 +0300

    List projects

--- projects/views.py
+++ projects/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
+from projects.models import Project, Role
+
 
 def projects_list(request):
+    projects_list = Project.objects.filter(roles__user=request.user)
     return render(request, 'projects_list.html', locals())
