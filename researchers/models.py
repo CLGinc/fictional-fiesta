d29f4440a8ad0476b42edbb8e65b216f617ee8db
George Goranov <g.p.goranov@gmail.com>
2016-10-13 00:08:15 +0300

    give proper plural name to university

--- researchers/models.py
+++ researchers/models.py
@@ -5,6 +5,9 @@ from django.db import models
 class University(models.Model):
     name = models.CharField(max_length=255)
 
+    class Meta:
+        verbose_name_plural = 'Universities'
+
     def __str__(self):
         return self.name
 
@@ -13,6 +16,7 @@ class Researcher(models.Model):
     user = models.OneToOneField(User, related_name='researcher')
     university = models.ForeignKey(University, related_name='researchers')
     scientific_degree = models.CharField(max_length=255)
+
 
     def __str__(self):
         return '{} {}'.format(self.scientific_degree, self.user.first_name, self.user.last_name)
