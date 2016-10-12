fa1720f4cf7925a71137f55c708472339f2f9601
George Goranov <g.p.goranov@gmail.com>
2016-10-12 23:22:58 +0300

    Move projects list extraction to Researcher model

--- researchers/models.py
+++ researchers/models.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+from projects.models import Project
+
 
 class University(models.Model):
     name = models.CharField(max_length=255)
@@ -20,3 +22,9 @@ class Researcher(models.Model):
 
     def __str__(self):
         return '{} {}'.format(self.scientific_degree, self.user.first_name, self.user.last_name)
+
+    def get_projects_by_roles(self, roles):
+        """
+        Returns projects list django query set based on a list of roles.
+        """
+        return Project.objects.filter(roles__user=self.user, roles__role__in=roles)
