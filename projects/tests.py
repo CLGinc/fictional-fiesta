17ae4990cda01f69f0a9f32d2473409db2fc2d8a
George Goranov <g.p.goranov@gmail.com>
2016-10-12 23:23:22 +0300

    Add basic get test for projects list view

--- projects/tests.py
+++ projects/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from django.test.client import Client
+from django.core.urlresolvers import reverse
 
-# Create your tests here.
+
+class ProjectsTest(TestCase):
+    fixtures = [
+        'researchers/fixtures/users',
+        'researchers/fixtures/researchers',
+        'researchers/fixtures/universities',
+        'projects/fixtures/projects',
+        'projects/fixtures/roles']
+
+    def setUp(self):
+        self.client = Client()
+
+    def test_get_projects_list(self):
+        self.client.login(username='user0', password='user0')
+        url = reverse('projets_list')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
