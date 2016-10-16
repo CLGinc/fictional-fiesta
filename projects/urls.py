ad1d1e09564db0c962f311562f8a4be53df99f1a
George Goranov <g.p.goranov@gmail.com>
2016-10-16 12:37:52 +0300

    add forward slash at the end of the url

--- projects/urls.py
+++ projects/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^list$', views.projects_list, name='projets_list'),
+    url(r'^list/$', views.projects_list, name='projets_list'),
 ]
