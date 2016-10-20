01870dd41bc4ace22dc853441952896524c98e7a
George Goranov <g.p.goranov@gmail.com>
2016-10-20 11:03:55 +0300

    rename role_names to roles

--- researchers/models.py
+++ researchers/models.py
@@ -22,7 +22,7 @@ class Researcher(models.Model):
     def __str__(self):
         return '{} {}'.format(self.scientific_degree, self.user.first_name, self.user.last_name)
 
-    def get_roles(self, scope=None, role_names=[role[0] for role in Role._meta.get_field('role').choices]):
+    def get_roles(self, scope=None, roles=[role[0] for role in Role._meta.get_field('role').choices]):
         """
         Returns roles list django query set based on scope and a list of role names.
         If no role names are specified all roles are selected.
@@ -31,16 +31,16 @@ class Researcher(models.Model):
         if scope == 'project':
             return Role.objects.filter(
                 user=self.user,
-                role__in=role_names,
+                role__in=roles,
                 protocol=None
                 ).exclude(project=None).select_related(scope)
         elif scope == 'protocol':
             return Role.objects.filter(
                 user=self.user,
-                role__in=role_names,
+                role__in=roles,
                 project=None
                 ).exclude(protocol=None).select_related(scope)
         else:
             return Role.objects.filter(
                 user=self.user,
-                role__in=role_names,).select_related('project', 'protocol')
+                role__in=roles,).select_related('project', 'protocol')
