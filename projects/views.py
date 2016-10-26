148998a76af6362fc330a9ce359950f20e1dcb5a
George Goranov <g.p.goranov@gmail.com>
2016-10-26 23:06:02 +0300

    add form for filter

--- projects/views.py
+++ projects/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
+from .models import Role
+
 
 def projects_list(request):
+    roles_labels = Role.ROLES
+    selected_roles = request.GET.getlist('role')
     if request.GET.getlist('role'):
         roles_list = request.user.researcher.get_roles(
             scope='project',
-            roles=request.GET.getlist('role'))
+            roles=selected_roles)
     else:
         roles_list = request.user.researcher.get_roles(scope='project')
     if request.GET.get('name'):
+        name_filter = request.GET.get('name')
         roles_list = roles_list.filter(project__name__contains=request.GET.get('name'))
     paginator = Paginator(roles_list, 15)
     page = request.GET.get('page')
