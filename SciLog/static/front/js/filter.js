4fdd89c5b9204f96c985644bd86b9269a63409bb
Martin Martev <mmartev@gmail.com>
2016-11-21 17:17:56 +0200

    initial add of datepicker

--- SciLog/static/front/js/filter.js
+++ SciLog/static/front/js/filter.js
@@ -5,3 +5,21 @@ $('#filter-button').click(function() {
 $('#new-project-button').click(function() {
   $('#new-project-form').toggle(200);
 });
+
+// Datepicker init
+var toggleButtonFrom = $('#input-filter--date-from'),
+      dialogFrom = new mdDateTimePicker.default({
+      type: 'date',
+      trigger: document.getElementById("input-filter--date-from")
+    }),
+    labelFrom = $('#date-from--label');
+
+toggleButtonFrom.click(function() {
+  dialogFrom.toggle();
+});
+
+toggleButtonFrom.on('onOk', function() {
+  dialogFrom.time = moment().format('YYYY-MM-DD');
+  this.value = dialogFrom.time.toString();
+  labelFrom.text(dialogFrom.time);
+});
