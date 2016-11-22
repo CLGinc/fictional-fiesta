9de5cf38d0ad2f8b3118f3d98c1df49c53ce6a14
George Goranov <g.p.goranov@gmail.com>
2016-11-22 15:06:15 +0200

    Add datetime_created for project

--- projects/models.py
+++ projects/models.py
@@ -20,6 +20,7 @@ class Project(models.Model):
     description = models.CharField(max_length=255, blank=True)
     sources = models.ManyToManyField('researchers.Source', related_name='projects', blank=True)
     protocols = models.ManyToManyField('protocols.Protocol', related_name='projects', blank=True)
+    datetime_created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
