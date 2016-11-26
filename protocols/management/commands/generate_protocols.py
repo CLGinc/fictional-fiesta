807646569ae5b5d0abf8c44dd08293fc10d38c3a
George Goranov <g.p.goranov@gmail.com>
2016-11-26 15:29:14 +0200

    add support for multiple projects

--- protocols/management/commands/generate_protocols.py
+++ protocols/management/commands/generate_protocols.py
@@ -41,6 +41,8 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         start_time = time.time()
+        logger = logging.getLogger('django')
+        logger.info('Start generating protocols')
         MEASUREMENTS = (
             ('Volume', 'litre'),
             ('Mass', 'kg'),
@@ -50,17 +52,21 @@ class Command(BaseCommand):
             ('Speed' 'm/s'),
             ('Speed' 'm/s'),
         )
-        logger = logging.getLogger('django')
         for protocol_idx in range(options['protocols']):
-            logger.info('Start generating protocols')
             # Preparations for connecting
             # protocol with projects and researchers
             researcher = random.choice(Researcher.objects.all())
-            add_to_project = random.choice((True, False))
-            if add_to_project:
-                project = random.choice(Project.objects.all())
+            add_to_projects = random.choice((True, False))
+            if add_to_projects:
+                number_of_projects = 0
+                while number_of_projects == 0:
+                    number_of_projects = random.randrange(
+                        Project.objects.count())
+                projects = random.sample(
+                    list(Project.objects.all()),
+                    number_of_projects)
             else:
-                project = None
+                projects = list()
             # Create protocol
             protocol = Protocol.objects.create(
                 name='Protocol {}'.format(protocol_idx),
@@ -89,7 +95,7 @@ class Command(BaseCommand):
                     state=random.choice(Result.STATES)[0],
                     is_successful=random.choice((True, False)),
                     protocol=protocol,
-                    project=project
+                    project=random.choice(projects) if add_to_projects else None
                 )
             # Create data columnd constructed from random data
             for data_idx in range(options['data_columns']):
@@ -111,15 +117,16 @@ class Command(BaseCommand):
                 protocol=protocol,
                 role=random.choice(Role.ROLES)[0]
             )
-            # Create random role between project and researcher
-            if add_to_project:
-                project.protocols.add(protocol)
-                if not(project.roles.filter(researcher=researcher)):
-                    project_role = Role.objects.create(
-                        researcher=researcher,
-                        project=project,
-                        role=random.choice(Role.ROLES)[0]
-                    )
-            execution_time = time.time() - start_time
-            logger.info("Finished! Execution time: {0:0.2f} seconds!".format(
+            # Create random role between projects and researcher
+            if add_to_projects:
+                for project in projects:
+                    project.protocols.add(protocol)
+                    if not(project.roles.filter(researcher=researcher)):
+                        project_role = Role.objects.create(
+                            researcher=researcher,
+                            projects=project,
+                            role=random.choice(Role.ROLES)[0]
+                        )
+        execution_time = time.time() - start_time
+        logger.info("Finished! Execution time: {0:0.2f} seconds!".format(
                 execution_time))
