6d7f7ae719853bb4c63decc4055009f9f393dd4e
Martin Martev <mmartev@gmail.com>
2016-11-30 15:33:24 +0200

    loader is no longer external template, finish loadOnClick and LoadOnScroll for ajax data

--- SciLog/static/front/js/infiniteScroll.js
+++ SciLog/static/front/js/infiniteScroll.js
@@ -8,19 +8,29 @@ var pageNum = 1, // The latest page loaded
     hasNextPage = true, // Indicates whether to expect another page after this one
     viewParam = $(location).attr('href'),
     datamode = $('#items_list').attr('data-mode'),
+    datalastpage = 1,
     win = $('main'),
     loadmorebtn = $("#loadmorebtn");
 
 $(window).load(function(){
-  if(datamode == 'infinitescroll'){
-    $(win).bind('scroll', loadOnScroll);
-    alert('infinitescroll');
-  } else if(datamode == 'loadmore') {
-    alert('loadmore');
-    $(loadmorebtn).bind('click', loadItems);
-  }
+  datalastpage = $('#items_list').attr('data-pages');
+  bindevent();
   return false;
 });
+var bindevent = function(){
+  if(datamode == 'infinitescroll'){
+    $(win).bind('scroll', loadOnScroll);
+  } else if(datamode == 'loadmore') {
+    $(loadmorebtn).bind('click', loadOnClick);
+  }
+};
+var unbindevent = function() {
+  if(datamode == 'loadmore'){
+    $(loadmorebtn).unbind();
+  } else if (datamode == 'infinitescroll') {
+    $(win).unbind();
+  }
+};
 
 // loadOnScroll handler
 var loadOnScroll = function() {
@@ -28,23 +38,32 @@ var loadOnScroll = function() {
     var element = event.target;
         if(element.scrollHeight - element.scrollTop === element.clientHeight) {
         // temporarily unhook the scroll event watcher so we don't call a bunch of times in a row
-        $(win).unbind();
+        unbindevent();
         // execute the load function below that will visit the JSON feed and stuff data into the HTML
         loadItems();
     }
 };
-
+// loadOnClick handler
+var loadOnClick = function(){
+    // temporarily unhook the click event watcher so we don't call a bunch of times in a row
+    unbindevent();
+    // execute the load function below that will visit the JSON feed and stuff data into the HTML
+    loadItems();
+};
 var loadItems = function() {
     $('#loading').addClass('is-active');
     // If the next page doesn't exist, just quit now
     if (hasNextPage === false) {
-        console.log('nonext');
         $('#loading').removeClass('is-active');
         return false
     }
+    if (pageNum == datalastpage) {
+      hasNextPage = false
+      $('#loading').removeClass('is-active');
+      return false
+    }
     // Update the page number
     pageNum = pageNum + 1;
-    console.log(pageNum+' '+viewParam+' '+hasNextPage);
     // Configure the url we're about to hit
     $.ajax({
         url: viewParam,
@@ -63,7 +82,7 @@ var loadItems = function() {
         },
         complete: function(data, textStatus){
             // Turn the scroll monitor back on
-            $(win).bind('scroll', loadOnScroll);
+            bindevent();
             $('#loading').removeClass('is-active');
         }
     });
