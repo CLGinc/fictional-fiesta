972a7226b15bf712116853a25717c779f0ec754c
Martin Martev <mmartev@gmail.com>
2016-12-01 16:43:39 +0200

    commit to new layout of project and remove unused js

--- SciLog/static/front/js/triggers.js
+++ SciLog/static/front/js/triggers.js
@@ -3,24 +3,6 @@ $('.scrollToTop-button').click(function(){
 	$(win).animate({scrollTop : 0},300);
 	return false;
 });
-// accordeons
-// $('#accordeon--protocols').hide();
-// $('[data-trigger="expand"]').click(function() {
-//   var targetElementId = $(this).attr('data-target');
-//   $('#'+targetElementId).toggle();
-//   console.log('click');
-// });
-// $('#accordeon--sources').hide();
-// $('[data-trigger="expand"]').click(function() {
-//   var targetElementId = $(this).attr('data-target');
-//   $('#'+targetElementId).slideToggle(250);
-//   console.log('click');
-// });
-// $('#accordeon--protocols').readmore({
-//   speed: 300,
-//   moreLink: '<div class="mdl-card__actions"><a class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-color-text--blue-grey-600"><i class="material-icons mdl-color-text--blue-500">expand_more</i>See all protocols</a></div>',
-//   lessLink: '<div class="mdl-card__actions"><a class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-color-text--blue-grey-600"><i class="material-icons mdl-color-text--blue-500">expand_more</i>See all protocols</a></div>'
-// });
 // filters
 $('[data-trigger="filter"]').click(function() {
   var targetElementId = $(this).attr('data-target');
