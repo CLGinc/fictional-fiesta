35e2fc3cc1dcf44f6a3b7919f034c33360cbebf1
George Goranov <g.p.goranov@gmail.com>
2016-12-04 12:08:59 +0200

    add command to generate researhers

---
+++ researchers/management/commands/generate_researchers.py
@@ -0,0 +1,34 @@
+import random
+import json
+import time
+import logging
+import uuid
+
+from django.core.management.base import BaseCommand
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+from researchers.models import Researcher
+
+
+class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '-r',
+            '--researchers',
+            default=100,
+            type=int,
+            help='Number of researchers. Default: 100')
+
+    def handle(self, *args, **options):
+        start_time = time.time()
+        logger = logging.getLogger('django')
+        logger.info('Start generating researchers')
+        for researcher_idx in range(options['researchers']):
+            User.objects.create(
+                username=uuid.uuid4(),
+                password=uuid.uuid4()
+            )
+        execution_time = time.time() - start_time
+        logger.info("Finished! Execution time: {0:0.2f} seconds!".format(
+                execution_time))
