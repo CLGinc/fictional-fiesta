7ef6dfdda818025a2910e9f4f79213a1b18bfd6b
Martin Martev <mmartev@gmail.com>
2016-12-05 16:37:52 +0200

    hide add button for results

--- SciLog/static/front/js/triggers.js
+++ SciLog/static/front/js/triggers.js
@@ -6,10 +6,24 @@ $('.scrollToTop-button').click(function(){
 // filters
 $('[data-trigger="filter"]').click(function() {
   var targetElementId = $(this).attr('data-target');
-  $('#'+targetElementId).toggle(200);
+  $('#'+targetElementId).slideToggle(200);
 });
 // expand
 $('[data-trigger="expand"]').click(function() {
   var targetElementId = $(this).attr('data-target');
   $('#'+targetElementId).toggleClass('accordeon--expand');
+});
+// update active tab and display add new button
+$('[data-trigger="tab"]').click(function() {
+  var activeTab = $(this).attr('data-target');
+  if(activeTab=='add-result-form'){
+    if(!$('#button-add').hasClass('hidden')){
+      $('#button-add').addClass('hidden');
+    }
+  } else {
+    if($('#button-add').hasClass('hidden')){
+      $('#button-add').removeClass('hidden');
+    };
+  }
+  $('#button-add').attr('data-target', activeTab);
 });
