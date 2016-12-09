65891ed92c2edd8c789a12906c96900812b70b5e
Martin Martev <mmartev@gmail.com>
2016-12-09 17:53:36 +0200

    add basic modals

--- SciLog/static/front/js/triggers.js
+++ SciLog/static/front/js/triggers.js
@@ -13,17 +13,33 @@ $('[data-trigger="expand"]').click(function() {
   var targetElementId = $(this).attr('data-target');
   $('#'+targetElementId).toggleClass('accordeon--expand');
 });
+// show
+$('[data-trigger="show"]').click(function(){
+  var targetElementId = $(this).attr('data-target');
+  $('#'+targetElementId).toggleClass('element--show-animate');
+});
+// close
+$('[data-trigger="close"]').click(function(){
+  var targetElementId = $(this).attr('data-target');
+  $('#'+targetElementId).removeClass('element--show-animate');
+});
+// submit
+$('[data-trigger="submit"]').click(function(){
+  var targetElementId = $(this).attr('data-target');
+  $('#'+targetElementId).removeClass('element--show-animate');
+  // DO SOME OTHER STUFF
+});
 // update active tab and display add new button
 $('[data-trigger="tab"]').click(function() {
   var activeTab = $(this).attr('data-target');
-  if(activeTab=='add-result-form'){
+  if(activeTab=='none'){
     if(!$('#button-add').hasClass('hidden')){
       $('#button-add').addClass('hidden');
     }
   } else {
     if($('#button-add').hasClass('hidden')){
       $('#button-add').removeClass('hidden');
-    };
+    }
   }
   $('#button-add').attr('data-target', activeTab);
 });
