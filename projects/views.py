4bb18a28883ec600c573614c013eda9fbbff6c39
George Goranov <g.p.goranov@gmail.com>
2016-12-12 21:15:25 +0200

    Code refactoring

--- projects/views.py
+++ projects/views.py
@@ -11,30 +11,7 @@ from .models import Project
 
 
 def projects_list(request):
-    roles_labels = Role.ROLES
-    selected_roles = request.GET.getlist('role')
-    if request.GET.getlist('role'):
-        roles_list = request.user.researcher.get_roles(
-            scope='project',
-            roles=selected_roles)
-    else:
-        roles_list = request.user.researcher.get_roles(scope='project')
-    if request.GET.get('name'):
-        name_filter = request.GET.get('name')
-        roles_list = roles_list.filter(
-            project__name__icontains=request.GET.get('name'))
-    if request.GET.get('created-from'):
-        created_from = datetime.strptime(
-            request.GET.get('created-from'),
-            '%Y-%m-%d').date()
-        roles_list = roles_list.filter(
-            project__datetime_created__date__gte=created_from)
-    if request.GET.get('created-to'):
-        created_to = datetime.strptime(
-            request.GET.get('created-to'),
-            '%Y-%m-%d').date()
-        roles_list = roles_list.filter(
-            project__datetime_created__date__lte=created_to)
+    # Handle new project creation
     if request.method == 'POST':
         new_project_form = NewProjectForm(request.POST or None)
         if new_project_form.is_valid():
@@ -46,17 +23,42 @@ def projects_list(request):
             new_project_role.save()
             return redirect(
                 '/projects/{}'.format(new_project_form.instance.unique_id))
-    paginator = Paginator(roles_list, 15)
-    roles_list_page = paginator.page(1)
-    if request.method == 'GET' and request.is_ajax():
-        page = request.GET.get('page')
-        try:
-            roles_list_page = paginator.page(page)
-        except PageNotAnInteger:
-            return HttpResponseBadRequest(reason='Page must be integer!')
-        except EmptyPage:
-            return HttpResponseBadRequest(reason='Page does not exist!')
-    return render(request, 'projects_list.html', locals())
+    elif request.method == 'GET':
+        roles_labels = Role.ROLES
+        selected_roles = request.GET.getlist('role')
+        if request.GET.getlist('role'):
+            roles_list = request.user.researcher.get_roles(
+                scope='project',
+                roles=selected_roles)
+        else:
+            roles_list = request.user.researcher.get_roles(scope='project')
+        if request.GET.get('name'):
+            name_filter = request.GET.get('name')
+            roles_list = roles_list.filter(
+                project__name__icontains=request.GET.get('name'))
+        if request.GET.get('created-from'):
+            created_from = datetime.strptime(
+                request.GET.get('created-from'),
+                '%Y-%m-%d').date()
+            roles_list = roles_list.filter(
+                project__datetime_created__date__gte=created_from)
+        if request.GET.get('created-to'):
+            created_to = datetime.strptime(
+                request.GET.get('created-to'),
+                '%Y-%m-%d').date()
+            roles_list = roles_list.filter(
+                project__datetime_created__date__lte=created_to)
+        paginator = Paginator(roles_list, 15)
+        roles_list_page = paginator.page(1)
+        if request.is_ajax():
+            page = request.GET.get('page')
+            try:
+                roles_list_page = paginator.page(page)
+            except PageNotAnInteger:
+                return HttpResponseBadRequest(reason='Page must be integer!')
+            except EmptyPage:
+                return HttpResponseBadRequest(reason='Page does not exist!')
+        return render(request, 'projects_list.html', locals())
 
 
 def project(request, project_id):
