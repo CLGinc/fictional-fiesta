b5e447d41cd191404343697ee0a9b20f613a37d9
Martin Martev <mmartev@gmail.com>
2016-12-14 15:00:59 +0200

    finalized modal design

--- SciLog/static/front/js/addDataRequest.js
+++ SciLog/static/front/js/addDataRequest.js
@@ -1,7 +1,16 @@
 $(window).load(function(){
   addDataRequest();
 });
-
+// Find checkbox state and toggle it
+var toggleCheckbox = function(){
+  var targetCheckbox = $(this).attr('data-target');
+  if($('#'+targetCheckbox).prop('checked')){
+    $('#'+targetCheckbox).prop('checked', false);
+  } else {
+    $('#'+targetCheckbox).prop('checked', true);
+  }
+};
+// Ajax request to retrieve list
 var addDataRequest = function() {
     $('#loading').addClass('is-active');
     // Configure the url we're about to hit
@@ -11,17 +20,17 @@ var addDataRequest = function() {
         type: "GET",
         dataType: 'html',
         success: function(data) {
-            console.log(data);
             // Update global next page variable
     				$('#protocols_to_add_list').append(data);
+            $('[data-trigger="checkbox"]').bind('click', toggleCheckbox);
         },
         error: function(data) {
             // When I get a 400 back, fail safely
-            hasNextPage = false
+            // bind a new event to the add button so the user can retry
         },
         complete: function(data, textStatus){
             // Turn the scroll monitor back on
-            bindevent();
+            // bind the event again so the user can open the list again
             $('#loading').removeClass('is-active');
         }
     });
