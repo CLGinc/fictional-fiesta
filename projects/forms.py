a1ea0f9ee84e53ca8500763903c3a6404c925961
George Goranov <g.p.goranov@gmail.com>
2016-12-17 15:07:06 +0200

    add forms for protocols/sources

--- projects/forms.py
+++ projects/forms.py
@@ -1,9 +1,25 @@
-from django.forms import ModelForm
+from django import forms
 
 from .models import Project
 
 
-class NewProjectForm(ModelForm):
+class NewProjectForm(forms.ModelForm):
     class Meta:
         model = Project
         fields = ['name', 'description']
+
+
+class AddElementsForm(forms.Form):
+    def __init__(self, *args, **kwargs):
+            queryset = kwargs.pop('queryset')
+            super(AddElementsForm, self).__init__(*args, **kwargs)
+            self.fields['element_choices'].queryset = queryset
+
+    CHOICES = (
+        ('p', 'Protocols'),
+        ('s', 'Sources'))
+    element_choices = forms.ModelMultipleChoiceField(
+        queryset=None,
+        widget=forms.CheckboxSelectMultiple
+    )
+    element_type = forms.ChoiceField(CHOICES)
