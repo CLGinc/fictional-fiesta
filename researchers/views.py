5f335710a344667fcac1f4169472b8bdd9562627
George Goranov <g.p.goranov@gmail.com>
2016-12-24 12:25:43 +0200

    add login and logout views

--- researchers/views.py
+++ researchers/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.conf import settings
+from django.core.urlresolvers import reverse
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, logout
 
-# Create your views here.
+
+def login_user(request):
+    if request.user.is_authenticated():
+        return redirect(reverse(settings.LOGIN_REDIRECT_URL))
+    form = AuthenticationForm(None, request.POST or None)
+    if request.method == 'POST':
+        if form.is_valid():
+            user = form.get_user()
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return redirect(reverse(settings.LOGIN_REDIRECT_URL))
+    return render(request, 'login.html', locals())
+
+
+def logout_user(request):
+    logout(request)
+    return redirect(reverse(settings.LOGOUT_REDIRECT_URL))
