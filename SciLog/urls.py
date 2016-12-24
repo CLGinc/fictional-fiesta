c774deaa9485465d1e8c1b31919f62e4fe9d9ac8
George Goranov <g.p.goranov@gmail.com>
2016-12-24 11:58:51 +0200

    import urls for researchers app

--- SciLog/urls.py
+++ SciLog/urls.py
@@ -19,4 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^projects/', include('projects.urls')),
+    url(r'^', include('researchers.urls')),
 ]
