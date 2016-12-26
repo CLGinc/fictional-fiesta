e852c4266bc34463bf6d357f63e18f0240721218
George Goranov <g.p.goranov@gmail.com>
2016-12-26 12:12:03 +0200

    add model structure

--- invitations/models.py
+++ invitations/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Invitation(models.Model):
+    email = mdoels.EmailField(unique=True, max_length=254)
+    inviter = models.ForeignKey(
+        'researchers.Researcher',
+        related_name='invitations'
+    )
+    invited = models.ForeignKey(
+        'researchers.Researcher',
+        related_name='invitations',
+        blank=True,
+        null=True
+    )
+    key = models.UUIDField(default=uuid.uuid4, editable=False)
+    expiration_days = models.PositiveSmallIntegerField(default=3)
+    datetime_created = models.DateTimeField(auto_now_add=True)
