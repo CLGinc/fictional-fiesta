01d942beb8127c0a6100b7122424811da34a7d25
George Goranov <g.p.goranov@gmail.com>
2016-12-28 12:35:30 +0200

    add basic accept view

--- invitations/views.py
+++ invitations/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import Http404
+from django.conf import settings
+from django.core.urlresolvers import reverse
 
-# Create your views here.
+from .models import Invitation
+
+
+def accept_invitation(request, key):
+    try:
+        invitation = Invitation.objects.get(key=key)
+    except Invitation.DoesNotExist:
+        raise Http404()
+    if request.method == 'GET':
+        if request.user.is_authenticated():
+            pass
+        else:
+            return redirect(reverse(settings.LOGIN_URL)+'?next={}'.format(invitation.key))
