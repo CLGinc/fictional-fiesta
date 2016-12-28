c66294673a9c14a1d2a911b6a719a3826774caf5
George Goranov <g.p.goranov@gmail.com>
2016-12-28 13:38:48 +0200

    calculate next target

--- invitations/views.py
+++ invitations/views.py
@@ -8,8 +8,10 @@ from .models import Invitation
 
 def accept_invitation(request, key):
     invitation = get_object_or_404(Invitation, key=key)
+    next_redirect = '?next={}'.format(reverse('accept_invitation', args=[key]))
+    print(next_redirect)
     if request.method == 'GET':
         if request.user.is_authenticated():
-            pass
+            return render(request, 'accept_invitation.html', locals())
         else:
-            return redirect(reverse(settings.LOGIN_URL)+'?next={}'.format(invitation.key))
+            return redirect(reverse(settings.LOGIN_URL)+next_redirect)
