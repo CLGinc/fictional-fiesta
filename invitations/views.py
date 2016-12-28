048cb958adddf36c10bf3434fb802b2eecb2520d
George Goranov <g.p.goranov@gmail.com>
2016-12-28 14:58:44 +0200

    add further invitation checks

--- invitations/views.py
+++ invitations/views.py
@@ -3,17 +3,20 @@ from django.shortcuts import get_object_or_404
 from django.conf import settings
 from django.core.urlresolvers import reverse
 from django.http import Http404
+from django.contrib.auth.decorators import login_required
 
 from .models import Invitation
 from .forms import AcceptInvitationForm
 
 
+@login_required
 def accept_invitation(request):
     key = request.GET.get('key') or request.POST.get('key')
     invitation = get_object_or_404(Invitation, key=key)
     if invitation.is_expired() or \
             invitation.inviter == request.user.researcher or \
-            invitation.accepted:
+            invitation.accepted or \
+            invitation.invited:
         raise Http404()
     form = AcceptInvitationForm({'key': key})
     if request.method == 'POST':
@@ -33,8 +36,4 @@ def accept_invitation(request):
                         args=[invitation.protocol.unique_id]
                     )
                 )
-    if request.user.is_authenticated():
-        return render(request, 'accept_invitation.html', locals())
-    else:
-        next_redirect = '?next={}?key={}'.format(reverse('accept_invitation'), key)
-        return redirect(reverse(settings.LOGIN_URL)+next_redirect)
+    return render(request, 'accept_invitation.html', locals())
