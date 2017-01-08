27144654b91308c5b7b5d53d1327df716b102006
Martin Martev <mmartev@gmail.com>
2017-01-09 00:09:28 +0200

    fix ajax again

--- SciLog/static/front/js/triggers.js
+++ SciLog/static/front/js/triggers.js
@@ -69,9 +69,8 @@ $('[data-trigger="submit"]').click(function(){
 
 $('[data-trigger="submit-ajax"]').click(function(){
   var targetForm = '#'+$(this).attr('data-form'),
-      url = viewParam + $(targetForm).attr('action'),
+      url = $(targetForm).attr('action'),
       formData = $(targetForm).serialize();
-			console.log(url);
 			$(targetForm).children('div').each(function(){
 				var emailInput = $(this).children("input[name='email']");
 				if(emailInput.val()){
