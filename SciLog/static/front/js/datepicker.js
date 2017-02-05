cec46a9b409de45abd783c7592058bb882913d9f
Martin Martev <mmartev@gmail.com>
2017-02-05 11:07:34 +0200

    fix label for filter

--- SciLog/static/front/js/datepicker.js
+++ SciLog/static/front/js/datepicker.js
@@ -1,11 +1,13 @@
 // Datepicker init and variables
-var toggleButtonFrom = document.getElementById('input-filter--date-from');
-var toggleButtonTo = document.getElementById('input-filter--date-to');
-var dialogFrom = new mdDateTimePicker.default({
+var toggleButtonFrom = document.getElementById('input-filter--date-from'),
+    toggleButtonFromLabel = $('#input-filter--date-from-label'),
+    toggleButtonTo = document.getElementById('input-filter--date-to'),
+    toggleButtonToLabel = $('#input-filter--date-to-label'),
+    dialogFrom = new mdDateTimePicker.default({
   type: 'date',
   trigger: toggleButtonFrom
-});
-var dialogTo = new mdDateTimePicker.default({
+}),
+  dialogTo = new mdDateTimePicker.default({
   type: 'date',
   trigger: toggleButtonTo
 });
@@ -15,6 +17,7 @@ toggleButtonFrom.addEventListener('click', function() {
   dialogFrom.toggle();
 });
 toggleButtonFrom.addEventListener('onOk', function() {
+  toggleButtonFromLabel.addClass('mdc-textfield__label--float-above');
   toggleButtonFrom.value = dialogFrom.time.format('YYYY-MM-DD').toString();
 });
 //  To date
@@ -22,5 +25,6 @@ toggleButtonTo.addEventListener('click', function() {
   dialogTo.toggle();
 });
 toggleButtonTo.addEventListener('onOk', function() {
+  toggleButtonToLabel.addClass('mdc-textfield__label--float-above');
   toggleButtonTo.value = dialogTo.time.format('YYYY-MM-DD').toString();
 });
