60970f8e96dbdcf0490ae5d65da1465b6ee36625
George Goranov <g.p.goranov@gmail.com>
2017-03-03 17:09:46 +0200

    add assign invitation view

--- invitations/urls.py
+++ invitations/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     url(r'^create/$', ajax.create_invitation, name='create_invitation'),
     url(r'^accept/$', ajax.accept_invitation, name='accept_invitation'),
     url(r'^list/$', views.InvitationsList.as_view(), name='invitations_list'),
+    url(r'^assign/(?P<slug>[a-zA-Z0-9]{64})/$', views.AssignInvitation.as_view(), name='assign_invitation'),
 ]
--- invitations/views.py
+++ invitations/views.py
@@ -1,6 +1,10 @@
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 from django.views.generic import ListView
+from django.views import View
+from django.views.generic.detail import SingleObjectMixin
+from django.shortcuts import redirect
+from django.core.urlresolvers import reverse
 
 from .models import Invitation
 
@@ -12,3 +16,19 @@ class InvitationsList(ListView):
 
     def get_queryset(self):
         return Invitation.objects.filter(invited=self.request.user.researcher)
+
+@method_decorator(login_required, name='dispatch')
+class AssignInvitation(SingleObjectMixin, View):
+    slug_field = 'key'
+
+    def get(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        if not(self.object.invited):
+            self.object.invited = self.request.user.researcher
+            self.object.save()
+        return redirect(reverse('invitations_list'))
+
+    def get_queryset(self):
+        return Invitation.objects.filter(
+            email=self.request.user.email
+        )
