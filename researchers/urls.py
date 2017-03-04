d29d9b53879d66f8a0d448fe62cfc057dc70ef52
George Goranov <g.p.goranov@gmail.com>
2017-03-04 16:26:53 +0200

    migrate login to class based view

--- researchers/urls.py
+++ researchers/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
 
 from . import views
+from .views import LoginView
 
 urlpatterns = [
-    url(r'^login/$', views.login_user, name='login_user'),
+    url(r'^login/$', LoginView.as_view(), name='login_user'),
     url(r'^logout/$', views.logout_user, name='logout_user'),
     url(r'^register/$', views.register_user, name='register_user'),
 ]
--- researchers/views.py
+++ researchers/views.py
@@ -2,27 +2,38 @@ from django.shortcuts import render, redirect
 from django.conf import settings
 from django.core.urlresolvers import reverse
 from django.contrib.auth import login, logout
+from django.views.generic.edit import FormView
 
 from .forms import EmailAuthenticationForm, EmailUserCreationForm
 from .models import Researcher
 
 
-def login_user(request):
-    next_redirect = request.GET.get('next')
-    if request.user.is_authenticated():
-        return redirect(reverse(settings.LOGIN_REDIRECT_URL))
-    form = EmailAuthenticationForm(data=request.POST or None)
-    if request.method == 'POST':
-        if form.is_valid():
-            user = form.get_user()
-            if user is not None:
-                if user.is_active:
-                    login(request, user, user.backend)
-                    if next_redirect:
-                        return redirect(next_redirect)
-                    else:
-                        return redirect(reverse(settings.LOGIN_REDIRECT_URL))
-    return render(request, 'login.html', locals())
+class LoginView(FormView):
+    template_name = 'login.html'
+    form_class = EmailAuthenticationForm
+
+    def form_valid(self, form):
+        user = form.get_user()
+        if user is not None:
+            if user.is_active:
+                login(self.request, user, user.backend)
+        return super(LoginView, self).form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        context = super(LoginView, self).get_context_data(**kwargs)
+        context['next_redirect'] = self.get_success_url()
+        return context
+
+    def get_success_url(self):
+        return self.request.GET.get(
+            'next',
+            reverse(settings.LOGIN_REDIRECT_URL)
+        )
+
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated():
+            return redirect(reverse(settings.LOGIN_REDIRECT_URL))
+        return super(LoginView, self).get(request, *args, **kwargs)
 
 
 def logout_user(request):
