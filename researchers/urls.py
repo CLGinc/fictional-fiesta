f1d9d0fb2e6fb83b9b19370879d50197fb56139d
George Goranov <g.p.goranov@gmail.com>
2017-03-04 16:49:35 +0200

    migrate register to class based view

--- researchers/urls.py
+++ researchers/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import url
 
-from .views import LoginView, LogoutView, register_user
+from .views import LoginView, LogoutView, RegisterView
 
 urlpatterns = [
     url(r'^login/$', LoginView.as_view(), name='login_user'),
     url(r'^logout/$', LogoutView.as_view(), name='logout_user'),
-    url(r'^register/$', register_user, name='register_user'),
+    url(r'^register/$', RegisterView.as_view(), name='register_user'),
 ]
--- researchers/views.py
+++ researchers/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import redirect
 from django.conf import settings
 from django.core.urlresolvers import reverse
 from django.contrib.auth import login, logout
@@ -45,19 +45,29 @@ class LogoutView(RedirectView):
         return super(LogoutView, self).get_redirect_url(*args, **kwargs)
 
 
-def register_user(request):
-    next_redirect = request.GET.get('next')
-    if request.user.is_authenticated():
-        return redirect(reverse(settings.REGISTER_REDIRECT_URL))
-    form = EmailUserCreationForm(data=request.POST or None)
-    if request.method == 'POST':
-        if form.is_valid():
-            form.save()
-            user = form.instance
-            Researcher.objects.create(user=user)
-            login(request, user, 'django.contrib.auth.backends.ModelBackend')
-            if next_redirect:
-                return redirect(next_redirect)
-            else:
-                return redirect(reverse(settings.REGISTER_REDIRECT_URL))
-    return render(request, 'register.html', locals())
+class RegisterView(FormView):
+    template_name = 'register.html'
+    form_class = EmailUserCreationForm
+
+    def form_valid(self, form):
+        form.save()
+        user = form.instance
+        Researcher.objects.create(user=user)
+        login(self.request, user, 'django.contrib.auth.backends.ModelBackend')
+        return super(RegisterView, self).form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        context = super(RegisterView, self).get_context_data(**kwargs)
+        context['next_redirect'] = self.get_success_url()
+        return context
+
+    def get_success_url(self):
+        return self.request.GET.get(
+            'next',
+            reverse(settings.REGISTER_REDIRECT_URL)
+        )
+
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated():
+            return redirect(reverse(settings.REGISTER_REDIRECT_URL))
+        return super(RegisterView, self).get(request, *args, **kwargs)
