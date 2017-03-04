22056a6bea54220b02b90442a6905744ebe2a061
George Goranov <g.p.goranov@gmail.com>
2017-03-04 19:03:15 +0200

    migrate get_protocols_to_add to class based

--- projects/ajax.py
+++ projects/ajax.py
@@ -1,9 +1,6 @@
 from django.http import HttpResponseForbidden
-from django.shortcuts import get_object_or_404
-from django.shortcuts import render
 from django.views.generic.base import TemplateView
 
-from .models import Project
 from .views import SingleProjectMixin
 
 
@@ -26,15 +23,20 @@ class GetProtocolsToAdd(TemplateView, SingleProjectMixin):
         return HttpResponseForbidden()
 
 
-def get_sources_to_add(request, project_uid):
-    if request.is_ajax() and request.method == 'GET':
-        selected_project = get_object_or_404(Project, unique_id=project_uid)
-        researcher = request.user.researcher
-        sources_to_add = researcher.get_sources_to_add(selected_project)
-        return render(
-            request,
-            'sources_to_add.html',
-            locals()
-        )
-    else:
+class GetSourcesToAdd(TemplateView, SingleProjectMixin):
+    template_name = 'sources_to_add.html'
+
+    def get_context_data(self, **kwargs):
+        self.object = self.get_object()
+        context = super(GetSourcesToAdd, self).get_context_data(**kwargs)
+        context['sources_to_add'] = \
+            self.request.user.researcher.get_sources_to_add(self.object)
+        return context
+
+    def get(self, request, *args, **kwargs):
+        if request.is_ajax():
+            return super(
+                GetSourcesToAdd,
+                self
+            ).get(request, *args, **kwargs)
         return HttpResponseForbidden()
--- projects/urls.py
+++ projects/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import url
 
-from . import views, ajax
-from .ajax import GetProtocolsToAdd
+from . import views
+from .ajax import GetProtocolsToAdd, GetSourcesToAdd
 
 urlpatterns = [
     url(r'^list/$', views.projects_list, name='projects_list'),
     url(r'^(?P<project_uid>[a-zA-Z0-9]{8})/$', views.project, name='project'),
     url(r'^(?P<project_uid>[a-zA-Z0-9]{8})/protocols_to_add/$', GetProtocolsToAdd.as_view(), name='project_get_protocols_to_add'),
-    url(r'^(?P<project_uid>[a-zA-Z0-9]{8})/sources_to_add/$', ajax.get_sources_to_add, name='project_get_sources_to_add'),
+    url(r'^(?P<project_uid>[a-zA-Z0-9]{8})/sources_to_add/$', GetSourcesToAdd.as_view(), name='project_get_sources_to_add'),
 ]
