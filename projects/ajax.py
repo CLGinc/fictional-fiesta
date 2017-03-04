5cbef3075cea23fe237d45614b1a428547b4cf10
George Goranov <g.p.goranov@gmail.com>
2017-03-04 19:18:44 +0200

    adde base class ItemsToAdd

--- projects/ajax.py
+++ projects/ajax.py
@@ -4,39 +4,39 @@ from django.views.generic.base import TemplateView
 from .views import SingleProjectMixin
 
 
-class GetProtocolsToAdd(TemplateView, SingleProjectMixin):
+class GetItemsToAdd(TemplateView, SingleProjectMixin):
+    context_key = ''
+
+    def get_context_data(self, **kwargs):
+        self.object = self.get_object()
+        context = super(GetItemsToAdd, self).get_context_data(**kwargs)
+        context[self.context_key] = \
+            self.get_items_to_add()
+        return context
+
+    def get(self, request, *args, **kwargs):
+        if request.is_ajax():
+            return super(
+                GetItemsToAdd,
+                self
+            ).get(request, *args, **kwargs)
+        return HttpResponseForbidden()
+
+    def get_items_to_add(self):
+        return None
+
+
+class GetProtocolsToAdd(GetItemsToAdd):
     template_name = 'protocols_to_add.html'
+    context_key = 'protocols_to_add'
 
-    def get_context_data(self, **kwargs):
-        self.object = self.get_object()
-        context = super(GetProtocolsToAdd, self).get_context_data(**kwargs)
-        context['protocols_to_add'] = \
-            self.request.user.researcher.get_protocols_to_add(self.object)
-        return context
-
-    def get(self, request, *args, **kwargs):
-        if request.is_ajax():
-            return super(
-                GetProtocolsToAdd,
-                self
-            ).get(request, *args, **kwargs)
-        return HttpResponseForbidden()
+    def get_items_to_add(self):
+        return self.request.user.researcher.get_protocols_to_add(self.object)
 
 
-class GetSourcesToAdd(TemplateView, SingleProjectMixin):
+class GetSourcesToAdd(GetItemsToAdd):
     template_name = 'sources_to_add.html'
+    context_key = 'sources_to_add'
 
-    def get_context_data(self, **kwargs):
-        self.object = self.get_object()
-        context = super(GetSourcesToAdd, self).get_context_data(**kwargs)
-        context['sources_to_add'] = \
-            self.request.user.researcher.get_sources_to_add(self.object)
-        return context
-
-    def get(self, request, *args, **kwargs):
-        if request.is_ajax():
-            return super(
-                GetSourcesToAdd,
-                self
-            ).get(request, *args, **kwargs)
-        return HttpResponseForbidden()
+    def get_items_to_add(self):
+        return self.request.user.researcher.get_sources_to_add(self.object)
