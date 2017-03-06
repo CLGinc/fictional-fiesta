a44d506100203762bbf71490c0f1584e1503a780
George Goranov <g.p.goranov@gmail.com>
2017-03-06 12:33:53 +0200

    url does not need 'projects' prefix

--- projects/urls.py
+++ projects/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     url(r'^list/$', ProjectList.as_view(), name='projects_list'),
     url(r'^create/$', CreateProject.as_view(), name='create_project'),
     url(r'^(?P<project_uid>[a-zA-Z0-9]{8})/$', ProjectView.as_view(), name='project'),
-    url(r'^(?P<project_uid>[a-zA-Z0-9]{8})/project_add_protocols/$', AddProtocols.as_view(), name='project_add_protocols'),
+    url(r'^(?P<project_uid>[a-zA-Z0-9]{8})/add_protocols/$', AddProtocols.as_view(), name='project_add_protocols'),
     url(r'^(?P<project_uid>[a-zA-Z0-9]{8})/add_sources/$', AddSources.as_view(), name='project_add_sources'),
 ]
