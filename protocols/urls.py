746f2090857f375f907d3b088f3579b73b65f948
George Goranov <g.p.goranov@gmail.com>
2017-03-09 18:12:18 +0200

    Add basic protocol view

--- protocols/urls.py
+++ protocols/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 
-from .views import ProtocoltList
+from .views import ProtocoltList, ProtocolView
 
 urlpatterns = [
     url(r'^list/$', ProtocoltList.as_view(), name='protocols_list'),
+    url(r'^(?P<protocol_uid>[a-zA-Z0-9]{8})/$', ProtocolView.as_view(), name='project'),
 ]
--- protocols/views.py
+++ protocols/views.py
@@ -1,8 +1,22 @@
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
+from django.core.urlresolvers import reverse
 from django.views.generic import ListView
+from django.views.generic.detail import SingleObjectMixin
+from django.views.generic.edit import UpdateView
 
 from researchers.views import RoleListMixin
+from .models import Protocol
+
+
+class SinglePrototolMixin(SingleObjectMixin):
+    slug_field = 'unique_id'
+    slug_url_kwarg = 'protocol_uid'
+
+    def get_queryset(self):
+        return Protocol.objects.filter(
+            roles__researcher=self.request.user.researcher
+        )
 
 
 @method_decorator(login_required, name='dispatch')
@@ -15,3 +29,15 @@ class ProtocoltList(ListView, RoleListMixin):
         if request.is_ajax():
             self.template_name = 'protocol_list_page.html'
         return super(ProtocoltList, self).get(request, *args, **kwargs)
+
+
+@method_decorator(login_required, name='dispatch')
+class ProtocolView(UpdateView, SinglePrototolMixin):
+    context_object_name = 'selected_protocol'
+    template_name = 'protocol.html'
+
+    def get_success_url(self):
+        return reverse(
+            'protocol',
+            kwargs={'protocol_uid': self.object.unique_id}
+        )
