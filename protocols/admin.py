a1be78cb3b9c3dc641cd0877912223ad14a8c267
George Goranov <g.p.goranov@gmail.com>
2017-03-10 16:17:37 +0200

    add order field

--- protocols/admin.py
+++ protocols/admin.py
@@ -5,9 +5,14 @@ from .models import Protocol, Asset, Procedure
 from .models import Step, Result, Attachment, DataColumn
 
 
+class InvitationAdmin(admin.ModelAdmin):
+    readonly_fields = ('order',)
+
+
 class StepInline(SortableStackedInline):
     model = Step
     extra = 1
+    readonly_fields = ('order',)
 
 
 class ProcedureAdmin(NonSortableParentAdmin):
@@ -17,7 +22,7 @@ class ProcedureAdmin(NonSortableParentAdmin):
 admin.site.register(Protocol)
 admin.site.register(Asset)
 admin.site.register(Procedure, ProcedureAdmin)
-admin.site.register(Step)
+admin.site.register(Step, InvitationAdmin)
 admin.site.register(Result)
 admin.site.register(Attachment)
 admin.site.register(DataColumn)
