9d838a1ad14d2dae5622f2140f7309b6b3a7f873
Martin Martev <mmartev@gmail.com>
2017-03-29 14:31:07 +0300

    remove inputs from protocol detail view, add url for edit protocol

--- protocols/urls.py
+++ protocols/urls.py
@@ -5,5 +5,6 @@ from .views import ProtocoltList, ProtocolView, CreateProtocol
 urlpatterns = [
     url(r'^list/$', ProtocoltList.as_view(), name='protocols_list'),
     url(r'^create/$', CreateProtocol.as_view(), name='create_protocol'),
+    url(r'^edit/$', CreateProtocol.as_view(), name='edit_protocol'),
     url(r'^(?P<protocol_uid>[a-zA-Z0-9]{8})/$', ProtocolView.as_view(), name='protocol'),
 ]
