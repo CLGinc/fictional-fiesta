14c8013b6b6d5423e38a4534119df4f85f86e33f
George Goranov <g.p.goranov@gmail.com>
2017-04-12 10:31:21 +0300

    Remove read only field

--- protocols/admin.py
+++ protocols/admin.py
@@ -4,14 +4,10 @@ from .models import Protocol, Asset, Procedure
 from .models import Step, Result, Attachment, DataColumn
 
 
-class InvitationAdmin(admin.ModelAdmin):
-    readonly_fields = ('order',)
-
-
 admin.site.register(Protocol)
 admin.site.register(Asset)
 admin.site.register(Procedure)
-admin.site.register(Step, InvitationAdmin)
+admin.site.register(Step)
 admin.site.register(Result)
 admin.site.register(Attachment)
 admin.site.register(DataColumn)
