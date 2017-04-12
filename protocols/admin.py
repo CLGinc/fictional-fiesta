b97232994364a5c983f29035f465aeec2f05ef7e
George Goranov <g.p.goranov@gmail.com>
2017-04-12 10:49:06 +0300

    Add inline steps for ease

--- protocols/admin.py
+++ protocols/admin.py
@@ -4,9 +4,19 @@ from .models import Protocol, Asset, Procedure
 from .models import Step, Result, Attachment, DataColumn
 
 
+class StepInline(admin.StackedInline):
+    model = Step
+
+
+class ProcedureAdmin(admin.ModelAdmin):
+    inlines = [
+        StepInline,
+    ]
+
+
 admin.site.register(Protocol)
 admin.site.register(Asset)
-admin.site.register(Procedure)
+admin.site.register(Procedure, ProcedureAdmin)
 admin.site.register(Step)
 admin.site.register(Result)
 admin.site.register(Attachment)
