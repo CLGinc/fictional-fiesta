75c81c253e62af691f607fe29fdf4728d19b984c
George Goranov <g.p.goranov@gmail.com>
2017-05-02 13:47:18 +0300

    Add DataColumnsFormset

--- protocols/forms.py
+++ protocols/forms.py
@@ -2,13 +2,22 @@ from django import forms
 from django.forms.models import inlineformset_factory
 
 from researchers.models import Role
-from.models import Protocol, Procedure, Step
+from.models import Protocol, Procedure, Step, Result, DataColumn
 
 
 StepsFormset = inlineformset_factory(
     parent_model=Procedure,
     model=Step,
     fields=('title', 'text', 'order'),
+    extra=0,
+    max_num=64,
+    min_num=1
+)
+
+DataColumnsFormset = inlineformset_factory(
+    parent_model=Result,
+    model=DataColumn,
+    fields=('data', 'is_independent', 'order', 'measurement', 'unit'),
     extra=0,
     max_num=64,
     min_num=1
