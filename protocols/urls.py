692d5237ebd5647d11812414fd32e866c1f1fcf9
George Goranov <g.p.goranov@gmail.com>
2017-05-02 13:14:28 +0300

    Add url for protocol result detail view

--- protocols/urls.py
+++ protocols/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls import url
 
 from .views import ProtocoltList, ProtocolView, CreateProtocol, UpdateProtocol
-from .views import CreateProtocolResult
+from .views import CreateProtocolResult, ProtocolResultView
 
 urlpatterns = [
     url(r'^list/$', ProtocoltList.as_view(), name='protocols_list'),
     url(r'^create/$', CreateProtocol.as_view(), name='create_protocol'),
     url(r'^(?P<protocol_uid>[a-zA-Z0-9]{8})/update/$', UpdateProtocol.as_view(), name='update_protocol'),
     url(r'^(?P<protocol_uid>[a-zA-Z0-9]{8})/$', ProtocolView.as_view(), name='protocol'),
-    url(r'^(?P<protocol_uid>[a-zA-Z0-9]{8})/result/create$', CreateProtocolResult.as_view(), name='create_protocol_result'),
+    url(r'^(?P<protocol_uid>[a-zA-Z0-9]{8})/result/create/$', CreateProtocolResult.as_view(), name='create_protocol_result'),
+    url(r'^(?P<protocol_uid>[a-zA-Z0-9]{8})/result/(?P<result_uid>[a-zA-Z0-9]{8})/$', ProtocolResultView.as_view(), name='protocol_result'),
 ]
