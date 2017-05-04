e1c55803ee384d15b3ae7cb22f442a34a51bccd8
George Goranov <g.p.goranov@gmail.com>
2017-05-04 12:35:22 +0300

    Add test_get_assets_by_category

--- protocols/tests.py
+++ protocols/tests.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from django.test.client import Client
 from django.core.exceptions import ValidationError
 
-from .models import Protocol, Result
+from .models import Protocol, Result, Asset
 from researchers.models import Researcher
 from projects.models import Project
 
@@ -26,6 +26,12 @@ class ProtocolsTest(TestCase):
         self.protocol2 = Protocol.objects.get(id=2)
         self.project1 = Project.objects.get(id=1)
         self.project2 = Project.objects.get(id=2)
+        self.asset1 = Asset.objects.get(id=1)
+        self.asset2 = Asset.objects.get(id=2)
+        self.asset3 = Asset.objects.get(id=3)
+        self.asset4 = Asset.objects.get(id=4)
+        self.asset5 = Asset.objects.get(id=5)
+        self.asset6 = Asset.objects.get(id=6)
 
     def test_create_result_when_not_owner_contributor_of_protocol(self):
         result = Result(
@@ -77,3 +83,12 @@ class ProtocolsTest(TestCase):
         self.assertEqual(
             ['The selected protocol does not belong to the selected project!'],
             e.exception.messages)
+
+    def test_get_assets_by_category(self):
+        assets = self.protocol1.get_assets_by_category()
+        materials = [a for a in assets[0][1]]
+        expected_materials = [self.asset1, self.asset2, self.asset3, self.asset5]
+        self.assertEqual(materials, expected_materials)
+        equipment = [a for a in assets[1][1]]
+        expected_equipment = [self.asset4, self.asset6]
+        self.assertEqual(equipment, expected_equipment)
