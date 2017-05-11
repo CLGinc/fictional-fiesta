195bbd2891c5184cfef1c22d139bcb6be8585dfc
George Goranov <g.p.goranov@gmail.com>
2017-05-11 16:39:24 +0300

    The conditions do not need negation since the queryset excludes

--- invitations/models.py
+++ invitations/models.py
@@ -69,9 +69,9 @@ class Invitation(models.Model):
         if self.project and self.protocol:
             raise ValidationError('You cannot select project and protocol for the same invitation!')
         if hasattr(self, 'inviter'):
-            if self.project and not(self.inviter.roles.filter(project=self.project).exclude(role__in=Role.ROLES_CAN_EDIT).exists()):
+            if self.project and self.inviter.roles.filter(project=self.project).exclude(role__in=Role.ROLES_CAN_EDIT).exists():
                 raise ValidationError('You cannot invite researchers to this project')
-            if self.protocol and not(self.inviter.roles.filter(protocol=self.protocol).exclude(role__in=Role.ROLES_CAN_EDIT).exists()):
+            if self.protocol and self.inviter.roles.filter(protocol=self.protocol).exclude(role__in=Role.ROLES_CAN_EDIT).exists():
                 raise ValidationError('You cannot invite researchers to this protocol')
         if hasattr(self, 'inviter') and hasattr(self, 'invited'):
             if self.inviter == self.invited:
