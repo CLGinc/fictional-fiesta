fda4b4b4b955388500a4ee8bac9dc699b90bcd60
Martin Martev <mmartev@gmail.com>
2017-05-30 18:04:36 +0300

    finish table update for create result

--- SciLog/static/front/js/datacolumns.js
+++ SciLog/static/front/js/datacolumns.js
@@ -9,19 +9,29 @@ $('[data-trigger="addDataRow"]').on('click', function() {
 $('[data-trigger="addDataColumn"]').on('click', function() {
   var sourceElement = $('[data-content="table--column"]').index(),
       colTemplate = '<col data-content="table--column"/>',
-      empyThTemplate = '<th></th>';
+      empyThTemplate = '<th></th>',
+      columnsCount = $('[data-content="table--column"]').length,
+      lastColumnRemoveButton = $('[data-content="table--controls-remove_col"]').last();
 
-  $('[data-content="table--dependant-variable"]').append(empyThTemplate);
   $('[data-content="table--column"]').last().after(colTemplate);
-  $('table tr').each(function(index) {
-    $('[data-content="table--row"]').append($("<td contenteditable='true'></td>"));
+  $('[data-content="table--labels"]').append(empyThTemplate);
+  $(removeColumnButton).insertAfter(lastColumnRemoveButton);
+  $('table tbody tr').each(function() {
+    var sourceTd = $(this).find('td:eq('+columnsCount+')'),
+        clonedTd = sourceTd.clone();
+    clonedTd.find('input').each(function(){
+      $(this).val('');
+    });
+    $(clonedTd).insertAfter(sourceTd);
   });
 });
 $('[data-content="dataTable--parent"]').on('click', '[data-trigger="removeDataRow"]', function() {
   var dataRowsCount = $('[data-content="table--row"]').length;
   if(dataRowsCount > 1) {
   var parentRow = $(this).closest('tr');
-  parentRow.remove();
+  $(parentRow).fadeOut("fast",function(){
+    $(this).remove();
+  });
   } else {
     var notif = 'At least one data row is required';
     show(snackbar,notif);
@@ -29,19 +39,27 @@ $('[data-content="dataTable--parent"]').on('click', '[data-trigger="removeDataRo
 });
 $('[data-content="dataTable--parent"]').on('click', '[data-trigger="removeDataColumn"]', function() {
   var dataColumnsCount = $('[data-content="table--column"]').length;
-  var parentColumn = $(this).index();
-  console.log(parentColumn);
+  var parentColumn = $(this).closest('th, td').index();
   if(dataColumnsCount > 1){
     var target_tr = $('table tr').find('td:eq('+parentColumn+'),th:eq('+parentColumn+')');
     $(target_tr).fadeOut("fast",function(){
         $(this).remove();
       });
+    $('[data-content="table--column"]').first().remove();
   } else {
     var notif = 'At least one data column is required';
     show(snackbar,notif);
   }
 });
 
-$('th, td').hover(function() {
- $(this).parents('table').find('col:eq('+$(this).index()+')').toggleClass('hover');
+// table highlights
+$('table').on('mouseenter', 'th, td', function() {
+  $(this).parents('table').find('col:eq('+$(this).index()+')').addClass('hover');
 });
+$('table').on('mouseleave', 'th, td', function() {
+  $(this).parents('table').find('col:eq('+$(this).index()+')').removeClass('hover');
+});
+
+// templates
+// remove col cell and button
+var removeColumnButton = '<th data-content="table--controls-remove_col"> <div data-mdc-auto-init="MDCRipple" data-trigger="removeDataColumn" class="mdc-button table--button"> Remove col </div></th>';
