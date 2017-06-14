2075bd4e772c402902d14f419bd31c02c2041fa1
George Goranov <g.p.goranov@gmail.com>
2017-06-14 17:05:34 +0300

    Add email association to the default pipeline

--- SciLog/settings.py
+++ SciLog/settings.py
@@ -148,6 +148,19 @@ if not(DEBUG):
 SOCIAL_AUTH_LOGIN_REDIRECT_URL = '/project/list'
 SOCIAL_AUTH_LOGIN_URL = '/'
 
+SOCIAL_AUTH_PIPELINE = (
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.social_auth.associate_by_email',  # Use email association to link existing users.
+    'social_core.pipeline.user.create_user',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+)
+
 # Google
 SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = os.environ.get('SOCIAL_AUTH_GOOGLE_OAUTH2_KEY',)
 SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = os.environ.get('SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET')
