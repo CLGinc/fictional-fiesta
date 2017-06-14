960c1b0d714397bb34998bf487e9bc2c85e08345
George Goranov <g.p.goranov@gmail.com>
2017-06-14 18:44:02 +0300

    Send mail if invited is not present

--- invitations/signals.py
+++ invitations/signals.py
@@ -1,8 +1,10 @@
+import json
+
 from django.db.models.signals import pre_save, post_save
 from django.dispatch import receiver
 from django.conf import settings
 from django.core.urlresolvers import reverse
-import json
+from django.core.mail import EmailMessage
 
 from .models import Invitation
 from users.models import Role
@@ -20,7 +22,7 @@ def set_invited(sender, instance, raw, using, update_fields, **kwargs):
 def send_mail(sender, created, instance, raw, using, update_fields, **kwargs):
     if not raw and created:
         variables = {
-            "invited": str(instance.invited),
+            "invited": str(instance.invited or instance.email),
             "inviter": str(instance.inviter),
             "object": {
                 "type": instance.get_item(),
@@ -29,12 +31,29 @@ def send_mail(sender, created, instance, raw, using, update_fields, **kwargs):
             "role": instance.role,
             "url": reverse('invitations_list')
         }
-        instance.invited.email_user(
-            template_id=settings.MJ_INVITATION_TEMPLATE_ID,
-            variables=json.dumps(variables),
-            from_email=settings.MJ_INVITATION_FROM,
-            fail_silently=settings.DEBUG
-        )
+        if instance.invited is not None:
+            instance.invited.email_user(
+                template_id=settings.MJ_INVITATION_TEMPLATE_ID,
+                variables=json.dumps(variables),
+                from_email=settings.MJ_INVITATION_FROM,
+                fail_silently=settings.DEBUG
+            )
+        else:
+            headers = {
+                'X-MJ-TemplateID': settings.MJ_INVITATION_TEMPLATE_ID,
+                'X-MJ-TemplateLanguage': '1',
+                'X-Mailjet-TrackClick': '1',
+                'X-Mailjet-TrackOpen': '1',
+                'X-MJ-Vars': json.dumps(variables)
+            }
+            email = EmailMessage(
+                subject='',
+                body='',
+                to=[instance.email],
+                from_email=settings.MJ_INVITATION_FROM,
+                headers=headers
+            )
+            email.send(fail_silently=settings.DEBUG)
 
 
 @receiver(post_save, sender=Invitation)
