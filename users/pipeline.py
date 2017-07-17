fe48131784413bd277ade74f5c61654f34d4e8ad
George Goranov <g.p.goranov@gmail.com>
2017-07-17 18:23:48 +0300

    Do not retrieve profile picture if one already exists

--- users/pipeline.py
+++ users/pipeline.py
@@ -3,12 +3,13 @@ from urllib.parse import urlparse
 
 def get_avatar(backend, strategy, details, response,
                user=None, *args, **kwargs):
-    url = None
-    if backend.name == 'google-oauth2':
-        url = urlparse(response['image'].get('url'))
-        url = '{}://{}{}'.format(url.scheme, url.netloc, url.path)
-    elif backend.name == 'facebook':
-        url = 'http://graph.facebook.com/{}/picture?type=large'.format(response['id'])
-    if url:
-        user.avatar = url
-        user.save()
+    if not user.avatar:
+        url = None
+        if backend.name == 'google-oauth2':
+            url = urlparse(response['image'].get('url'))
+            url = '{}://{}{}'.format(url.scheme, url.netloc, url.path)
+        elif backend.name == 'facebook':
+            url = 'http://graph.facebook.com/{}/picture?type=large'.format(response['id'])
+        if url:
+            user.avatar = url
+            user.save()
