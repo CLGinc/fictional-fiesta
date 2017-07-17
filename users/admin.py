675b0b857c05c0eb4816791d3d3806a59d9381b7
George Goranov <g.p.goranov@gmail.com>
2017-07-17 12:41:58 +0300

    Add scientific_degree to admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,4 @@
+from django.utils.translation import gettext_lazy as _
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
@@ -18,6 +19,19 @@ class SourceAdmin(admin.ModelAdmin):
     list_display = ('name', 'user', 'isbn', 'url')
 
 
-admin.site.register(User, UserAdmin)
+class ExtendedUserAdmin(UserAdmin):
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'email',
+                                         'scientific_degree')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
+                                       'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff',
+                    'scientific_degree')
+
+
+admin.site.register(User, ExtendedUserAdmin)
 admin.site.register(Role, RoleAdmin)
 admin.site.register(Source, SourceAdmin)
