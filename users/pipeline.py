c9ecf58f0ec01471a1b8b5c47e4193ee9385ca87
George Goranov <g.p.goranov@gmail.com>
2017-07-18 17:36:02 +0300

    Update avatar url if different from the one in db

--- users/pipeline.py
+++ users/pipeline.py
@@ -3,13 +3,12 @@ from urllib.parse import urlparse
 
 def get_avatar(backend, strategy, details, response,
                user=None, *args, **kwargs):
-    if not user.avatar:
-        url = None
-        if backend.name == 'google-oauth2':
-            url = urlparse(response['image'].get('url'))
-            url = '{}://{}{}'.format(url.scheme, url.netloc, url.path)
-        elif backend.name == 'facebook':
-            url = 'http://graph.facebook.com/{}/picture'.format(response['id'])
-        if url:
-            user.avatar = url
-            user.save()
+    url = None
+    if backend.name == 'google-oauth2':
+        url = urlparse(response['image'].get('url'))
+        url = '{}://{}{}'.format(url.scheme, url.netloc, url.path)
+    elif backend.name == 'facebook':
+        url = 'http://graph.facebook.com/{}/picture'.format(response['id'])
+    if url and user.avatar != url:
+        user.avatar = url
+        user.save()
